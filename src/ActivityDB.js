830f9b9cc4a87fe8778b8668493e76c62243acea
Matteriel-Git <98388545+Matteriel-Git@users.noreply.github.com>
2023-03-21 13:13:25 -0700

    Add files via upload

---
+++ src/ActivityDB.js
@@ -0,0 +1,204 @@
+const activities = [
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+    {
+        "activity": "",
+        "description": ""
+    },
+]
+
+export default activities;
---
+++ src/components/ActivityAndDescription.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import activities from '../ActivityDB'
+
+export default function ActivityAndDescription(props) {
+
+    const { activity, generateRandomActivity } = props;
+    return (
+        <div className="card" >
+
+            <div className="card-body">
+                <p className="card-text">{activity.activity}</p>
+                <h5 className="card-title">- {activity.description}</h5>
+                <button
+                onClick={() => {generateRandomActivity(activities) }}
+                type="submit">
+                <i className="fas fa-mouse"></i> Generate Quote</button>
+                <button
+                className="m1-3"
+                onClick={() => {
+                    generateRandomActivity(activity);
+                    window.open('https://twitter.com/intent/tweet/?text=' + encodeURIComponent(activity.activity + '--' + activity.description))
+                }}
+                type="submit"><i class="fab fa-twitter"></i> Share Your New Activity</button>
+            </div>
+        </div>
+    )
+}
