f0b688ad39b5c673eb637a58dda915f0145ad6e2
Matteriel-Git <98388545+Matteriel-Git@users.noreply.github.com>
2023-03-21 12:14:12 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react'
+import ActivityAndDescription from './components/ActivityAndDescription';
+import activities from './ActivityDB';
+
+export default class App extends Component {
+
+  // state
+  state = {
+    activity: activities[0].activity,
+    description: activities[0].description
+  }
+
+  // generate different activity function
+  
+
+}
