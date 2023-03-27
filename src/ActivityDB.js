d5b9c3f06956df20b7c83a68b823f575d9850ad8
Matteriel-Git <98388545+Matteriel-Git@users.noreply.github.com>
2023-03-27 13:24:14 -0700

    Add files via upload

--- src/ActivityDB.js
+++ src/ActivityDB.js
@@ -2,252 +2,252 @@ const activities = [
     {
         "activity": "Go for a walk",
         "description": "To walk for pleasure rather than for practical reasons",
-        "picture": <img src="https://www.henryford.com/-/media/project/hfhs/henryford/henry-ford-blog/images/interior-banner-images/2019/03/walking-for-health-1.jpg?h=785&iar=0&w=1920&rev=fb6e2e7cb2c44ee3b10effde44402f8d&hash=8118FF4C98DF2B01496A59E32CC7A4B2" alt="walking" />
+        "picture": <img src="images/walk.jpg" alt="walk" />
     },
     {
         "activity": "Listen to music",
         "description": "Put on your favortite music",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/listen.jpg" alt="earbuds" />
     },
     {
         "activity": "Photography",
         "description": "Take pictures of something that interests you. Can be fashion, sports, architecture, nature, etc",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/photography.jpeg" alt="camera" />
     },
     {
         "activity": "Read a newspaper or magazine",
         "description": "A printed publication (usually issued daily or weekly) consisting of folded unstapled sheets and containing news, feature articles, advertisements, and correspondence",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/newspaper.jpg" alt="newspaper" />
     },
     {
         "activity": "Take a bath",
         "description": "Relax in a nice long bath",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Sit in the sun",
         "description": "Sunscreen required",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Watch a movie",
         "description": "An old favorite, or a new one that interests you",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Go on a bike ride",
         "description": "Find a nice path and explore it",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Write in a journal",
         "description": "Put your thoughts down on paper",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Work on a puzzle",
         "description": "A game, toy, or problem designed to test ingenuity or knowledge",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Recall a happy memory",
         "description": "The best memory you can think of",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Coloring",
         "description": "Grab a marker or colored pencil and start coloring",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Gardening",
         "description": "Grow some plants",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Spend time with friends",
         "description": "Have some fun with your firends",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Do yoga",
         "description": "A Hindu spiritual and ascetic discipline, a part of which, including breath control, simple meditation, and the adoption of specific bodily postures, is widely practiced for health and relaxation",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Swimming",
         "description": "Great activity for a hot day",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Singing",
         "description": "Perform some songs",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Dancing",
         "description": "Get your feet and body moving to some music",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Go to a flea market",
         "description": "Full of interesting people and things",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Drawing",
         "description": "A picture or diagram made with a pencil, pen, or crayon rather than paint",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Painting",
         "description": "The process or art of using paint, in a picture, as a protective coating, or as decoration",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Go running",
         "description": "The action or movement of a runner",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Join a book club",
         "description": "Where a group comes together to talk about the current book they are reading",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Go to a farmer's market",
         "description": "A food market at which local farmers sell fruit and vegetables and often meat, cheese, and bakery products directly to consumers.",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Talk to loved one's",
         "description": "Could be a family member or someone you care deeply about",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Play a musical instrument",
         "description": "Any device for producing a musical sound",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Crafting (crochet, model building)",
         "description": "The activity or hobby of making decorative articles by hand",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Get a massage",
         "description": "The rubbing and kneading of muscles and joints of the body with the hands, especially to relieve tension or pain",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Cooking",
         "description": "The practice or skill of preparing food by combining, mixing, and heating ingredients",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Go to a park",
         "description": "A large public green area in a town, used for recreation",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Play a sport",
         "description": "Football, basketball, baseball, soccer, etc",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Go sightseeing in your own town",
         "description": "The activity of visiting places of interest in a particular location",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Read a book",
         "description": "A book you've always wanted to read",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Meditate",
         "description": "Think deeply or focus one's mind for a period of time, in silence or with the aid of chanting, for religious or spiritual purposes or as a method of relaxation",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Watch a sunrise or sunset",
         "description": "Great to be paired with another acitivity on this list",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Enjoy a cup of tea",
         "description": "A hot drink made by infusing the dried crushed leaves of the tea plant in boiling water",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Have a picnic",
         "description": "An outing or occasion that involves taking a packed meal to be eaten outdoors",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Visit a museum",
         "description": "A building in which objects of historical, scientific, artistic, or cultural interest are stored and exhibited",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Sudoku or a crossword puzzle",
         "description": "A puzzle in which players insert the numbers one to nine into a grid consisting of nine squares subdivided into a further nine smaller squares in such a way that every number appears once in each horizontal line, vertical line, and square",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Play a board game",
         "description": "Any game played on a board, especially one that involves the movement of pieces on the board, such as chess or checkers",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Look at old photos",
         "description": "One's that bring back great memories",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Roadtrip",
         "description": "A journey made by car, bus, etc",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Call a friend or family member",
         "description": "Just to catch up with each other",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Hiking",
         "description": "The activity of going for long walks, especially in the country or woods",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Go to movie theatre",
         "description": "A theater where movies are shown for public entertainment",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Volunteer",
         "description": "A person who freely offers to take part in an enterprise or undertake a task",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Go to the library",
         "description": "A building or room containing collections of books, periodicals, and sometimes films and recorded music for people to read, borrow, or refer to",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Go to a coffee shop",
         "description": "A cafe serving coffee and light refreshments",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Stargazing",
         "description": "Observing the stars",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     },
     {
         "activity": "Explore somewhere new",
         "description": "Anywhere that interests you",
-        "picture": <img src="" alt="" />
+        "picture": <img src="images/" alt="" />
     }
 ]
 
